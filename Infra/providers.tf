1c514fd20091c20d48587bfdcddd58c01191315d
Rick G <a@b.com>
2025-08-05 16:33:54 -0700

    inital working commit of the infra component

--- Infra/providers.tf
+++ Infra/providers.tf
@@ -2,9 +2,9 @@
 # Configure Providers
 # ----------------------------------------------------------------------------------------------------------------------
 provider "google" {
-    credentials = file("/home/admin/.config/gcloud/application_default_credentials.json")
+
 }
 
 provider "google-beta" {
-    credentials = file("/home/admin/.config/gcloud/application_default_credentials.json")
+
 }
